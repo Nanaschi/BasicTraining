8e5ff749a78f50590e4f59e5cea4b7f8c24484fc
Dani <f1gasl74r1@gmail.com>
2022-06-05 08:12:38 +0300

    Initial commit

---
+++ ConsoleApp/PlayersContainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class PlayersContainer
+    {
+        private List<string> names;
+        private string[] lastNames;
+        public PlayersContainer()
+        {
+            ListIntialization();
+            ArrayIntialization();
+            FullName();
+        }
+
+        private void ArrayIntialization()
+        {
+            lastNames = new []
+            {
+                "Franklin", "O'Brian", "Mc Gregor"
+            };
+        }
+
+        private void ListIntialization()
+        {
+            names = new List<string>()
+            {
+                "Bob", "Sam", "Pol"
+            };
+        }
+
+        private void FullName()
+        {
+            string[] joinednames = names.Zip
+                (lastNames, (name, lastName) => $"{name} {lastName}").ToArray();
+            foreach (var VARIABLE in joinednames)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+
+        static void Main()
+        {
+            new PlayersContainer();
+        }
+        
+    }
+}
